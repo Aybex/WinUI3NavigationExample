a1d0ff26df73435f66a5cd2dd4fe656b74a0ea11
Ayoub SAHI <ayoubsahi@outlook.com>
2023-10-24 02:54:24 +0100

    styles and mvvm

---
+++ WinUI3NavigationExample/WinUI3NavigationExample/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+namespace WinUI3NavigationExample.ViewModels;
+
+public partial class HomeViewModel: ObservableObject
+{
+    [ObservableProperty] private string name = "Home Page Name";
+    [ObservableProperty] private bool buttonEnabled = true;
+
+}
--- WinUI3NavigationExample/WinUI3NavigationExample/Views/HomePage.xaml.cs
+++ WinUI3NavigationExample/WinUI3NavigationExample/Views/HomePage.xaml.cs
@@ -1,18 +1,20 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using WinUI3NavigationExample.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace WinUI3NavigationExample.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class HomePage : Page
     {
+        public HomeViewModel ViewModel { get; } = new();
+
         public HomePage()
         {
             InitializeComponent();
+            this.DataContext = ViewModel;
         }
     }
 }
